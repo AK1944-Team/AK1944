4ba13633c8be64818ea25aa3bb4fe3a500697966
{Agata Kuklińska} <delicadency@gmail.com>
2025-10-09 21:09:32 +0200

    chore: format jest.config.js for improved readability

--- jest.config.js
+++ jest.config.js
@@ -6,20 +6,36 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   setupFilesAfterEnv: [
-    "@testing-library/jest-dom", // Zaktualizowano do poprawnej ścieżki
-    "<rootDir>/jest.setup.js", // Dodatkowy plik setup (opcjonalnie)
+    "@testing-library/jest-dom",
+    "<rootDir>/jest.setup.js",
   ],
+
   testEnvironment: "jest-fixed-jsdom",
+
   testEnvironmentOptions: {
     customExportConditions: [""],
   },
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/node_modules/",
+  ],
+
   transform: {
     "^.+\\.(ts|tsx|js|jsx|mjs)$": "ts-jest",
   },
+
+  transformIgnorePatterns: [
+    "node_modules/(?!(msw|until-async)/)"
+  ],
+
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
+
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+
+  verbose: false,
 };
 
 export default createJestConfig(customJestConfig);
