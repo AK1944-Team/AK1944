a90a58dd4fa172ed4af69252a77955997d9b1bf2
{Agata Kuklińska} <delicadency@gmail.com>
2025-10-09 21:00:21 +0200

    chore: simplify next.config.ts and update Storybook dependencies to version 9.1.10

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      { hostname: "ak1944.pl" },
-      { hostname: "wp.ak1944.pl" },
-    ],
-  },
-
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.stories\.(js|jsx|ts|tsx)$/,
-      loader: "ignore-loader",
-    });
-    return config;
+    remotePatterns: [{ hostname: "ak1944.pl" }, { hostname: "wp.ak1944.pl" }],
   },
 };
 
