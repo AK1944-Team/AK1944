19554f501db62d7fc23257ed4b8fb162433cb9a5
Vincent Słomiński <vincentslominski@gmail.com>
2026-07-18 14:30:49 +0200

    Feat: Update Partner and PartnerCard components to make href optional and improve rendering logic

--- src/app/(client)/partnerzy/_components/Carousel/PartnerCard.tsx
+++ src/app/(client)/partnerzy/_components/Carousel/PartnerCard.tsx
@@ -2,32 +2,41 @@ import Image from "next/image";
 
 interface Props {
   name: string;
-  href: string;
+  href?: string | null;
   image?: string;
 }
 
 const SIZE = 320;
 
+const Content = ({ name, image }: { name: string; image?: string }) =>
+  image ? (
+    <Image
+      className="max-h-80 max-w-80"
+      src={image}
+      alt={name}
+      title={name}
+      width={SIZE}
+      height={SIZE}
+    />
+  ) : (
+    <h5 className="text-center text-xl font-bold text-greenMain">{name}</h5>
+  );
+
 export const PartnerCard = ({ name, href, image }: Props) => (
   <article className="flex h-full w-full items-center justify-center overflow-hidden p-2 contrast:bg-[#FFFFFF]">
-    <a
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="max-h-80 max-w-80"
-    >
-      {image ? (
-        <Image
-          className="max-h-80 max-w-80"
-          src={image}
-          alt={name}
-          title={name}
-          width={SIZE}
-          height={SIZE}
-        />
-      ) : (
-        <h5 className="text-center text-xl font-bold text-greenMain">{name}</h5>
-      )}
-    </a>
+    {href ? (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="max-h-80 max-w-80"
+      >
+        <Content name={name} image={image} />
+      </a>
+    ) : (
+      <div className="max-h-80 max-w-80">
+        <Content name={name} image={image} />
+      </div>
+    )}
   </article>
 );
